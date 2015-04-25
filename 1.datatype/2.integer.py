291cb41904b2a7d22dd35abcba100a62d5fa1ac8
codelectron <codelectron@gmail.com>
2015-04-25 23:17:00 +0200

    Integer code

--- 1.datatype/2.integer.py
+++ 1.datatype/2.integer.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python
+import sys
 
 print("Python Integer")
-a=10
-b=20
-c=-10
+print sys.maxsize
+
+print type(sys.maxsize )
+print type(sys.maxsize +1 )
+
+a = 10
+b = 20
+c = -10
+
 print type(a)
+print isinstance(a, bool)
 print max(a,b)
 print max(a,c)
 print cmp(a,b)
