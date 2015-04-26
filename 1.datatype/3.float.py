88ecf17306cd2deb3e373f3959eea9fe009a6071
codelectron <codelectron@gmail.com>
2015-04-26 16:16:25 +0200

    Add Float

---
+++ 1.datatype/3.float.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import math
+
+print 0.12345555+0.4321
+a = 0.1
+print type(a)
+b = 10
+print type(b)
+print b
+print("Converting int to float")
+b = float(b)
+print type(b)
+print b
+
+
+print "Value of PI is "+ repr(math.pi)
+piVal = math.pi
+print type(piVal)
+piprecise = "%.5f"%piVal
+print piprecise
+print type(piprecise)
+piprecise  = float(piprecise)
+print piprecise
+print type(piprecise)
